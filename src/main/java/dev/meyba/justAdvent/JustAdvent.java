389c06a28ca4b0ace1b700fe7e815ab6a3eeab9f
rokyytr.official <163911146+RokyYTR2@users.noreply.github.com>
2025-12-07 13:24:23 +0100

    Init

---
+++ src/main/java/dev/meyba/justAdvent/JustAdvent.java
@@ -0,0 +1,12 @@
+package dev.meyba.justAdvent;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class JustAdvent extends JavaPlugin {
+
+    @Override
+    public void onEnable() {}
+
+    @Override
+    public void onDisable() {}
+}
